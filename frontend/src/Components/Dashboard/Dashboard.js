d3b3a9dc9863c558cb5e223ed4738f9f449587ff
sanjana3339 <sanjana.mudimala@yahoo.com>
2024-09-14 17:52:18 +0530

    added dashboard chart

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -1,17 +1,69 @@
-import './Dashboard.css';
-import React  from 'react';
+import React, { useEffect } from 'react'
 import '../../Layout.css';
+import './Dashboard.css';
+import { useGlobalContext } from '../../Context/globalContext';
+import Chart from '../Chart/Chart';
+
 function Dashboard() {
+    const {totalExpense,incomes, expenses, totalIncome, totalBalance, getIncomes, getExpense} = useGlobalContext()
 
-  return (
-    <div className="Dashboard">
-      <div className='Inner-layout'>
-      <h1>Dashboard</h1>
-      </div>
-      
-    </div>
-  );
+    useEffect(() => {
+        getIncomes()
+        getExpense()
+    }, [])
+
+    return (
+        <div className='dashboard'>
+            <div className='Inner-layout'>
+                <h1>All Transactions</h1>
+                <div className="stats-con">
+                    <div className="chart-con">
+                        <Chart />
+                        <div className="amount-con">
+                            <div className="income">
+                                <h2>Total Income</h2>
+                                <p>
+                                     {totalIncome()}
+                                </p>
+                            </div>
+                            <div className="expense">
+                                <h2>Total Expense</h2>
+                                <p>
+                                     {totalExpense()}
+                                </p>
+                            </div>
+                            <div className="balance">
+                                <h2>Total Balance</h2>
+                                <p>
+                                    {totalBalance()}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history-con">
+                        <h2 className="salary-title">Min <span>Salary</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                ${Math.min(...incomes.map(item => item.amount))}
+                            </p>
+                            <p>
+                                ${Math.max(...incomes.map(item => item.amount))}
+                            </p>
+                        </div>
+                        <h2 className="salary-title">Min <span>Expense</span>Max</h2>
+                        <div className="salary-item">
+                            <p>
+                                ${Math.min(...expenses.map(item => item.amount))}
+                            </p>
+                            <p>
+                                ${Math.max(...expenses.map(item => item.amount))}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
-
 
 export default Dashboard;
